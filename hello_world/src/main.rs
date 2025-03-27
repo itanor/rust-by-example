daf37452db0624d92e11b82d1989b5c7c92be99a
Itanor Strapazzon <itanor@gmail.com>
2025-03-26 21:14:26 -0300

    hello world formatting

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::fmt::{self, Formatter, Display};
 
 fn formatted_print() {
     println!("{} days", 31);
@@ -127,9 +127,55 @@ fn testcase_list() {
     println!("{}", v);
 }
 
+fn formatting() {
+    struct City {
+        name: &'static str,
+        lat: f32,
+        lon: f32,
+    }
+    impl Display for City {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            let lat_c = if self.lat >= 0.0 {'N'} else {'S'};
+            let lon_c = if self.lon >= 0.0 {'E'} else {'W'};
+
+            write!(f, "{}: {:.3}° {} {:.3}° {}", 
+                self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+        }
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+    impl Display for Color {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "r: {}, g: {}, b: {}", self.red, self.green, self.blue)
+        }
+    }
+
+    for city in [
+        City {name: "Dublin", lat: 53.347778, lon: -6.259722},
+        City {name: "Oslo",lat: 59.95, lon: 10.75 },
+        City {name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ] {
+        println!("{}", city);
+    }
+
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ] {
+        println!("{}", color);
+    }
+}
+
 fn main() {
     formatted_print();
     debug();
     display();
     testcase_list();
+    formatting();
 }
