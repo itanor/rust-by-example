9b605fa9607cca8c158d9290891e8c57d0544f0b
Itanor Strapazzon <itanor@gmail.com>
2025-04-16 23:54:47 -0300

    literals and operators

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -1,3 +1,23 @@
+fn literals_and_operators() {
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    println!("1 - 2 = {}", 1i32 - 2);
+
+    println!("1e4 is {} -2.5e-3 is {}", 1e4, -2.5e-3);
+
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    println!("One million is is written as {}", 1_000_000u32);
+}
+
 fn main() {
     let logical: bool = true;
     println!("{}", logical);
@@ -24,4 +44,6 @@ fn main() {
 
     let my_tuple = (5u32, 1u8, true, -5.04f32);
     println!("{:?}", my_tuple);
+
+    literals_and_operators();
 }
