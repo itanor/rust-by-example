0075ee7de7dcc8817ee41c1fd489e0d1b8afe55e
Itanor Strapazzon <itanor@gmail.com>
2025-09-03 21:31:03 -0300

    more that 1 binary on project

---
+++ cargo_conventions/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("this project has two binaries!");
+    println!("one is this main!");
+    println!("another is located in src/bin!");
+    println!("also have a Makefile!");
+}
